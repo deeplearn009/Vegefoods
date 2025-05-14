3ec11fa7640dbbda655e834b96d57e0d3bb814e4
deeplearn009 <sragimov@vk.com>
2025-05-14 23:10:48 +0400

    ff

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styles from './Header.module.scss'
 import {Link} from "react-router";
-import {FaShop} from "react-icons/fa6";
+import {FaBurger, FaShop} from "react-icons/fa6";
 import {FaShoppingCart} from "react-icons/fa";
+import {CiMenuBurger} from "react-icons/ci";
 
 const Header = () => {
     return (
@@ -20,6 +21,9 @@ const Header = () => {
                 </nav>
                 <div className={styles.icon}>
                     <Link to='/basket' id={styles.icon}><FaShoppingCart size={11}/></Link>
+                </div>
+                <div className={styles.burger}>
+                    <CiMenuBurger size={25}/>
                 </div>
             </header>
         </div>
